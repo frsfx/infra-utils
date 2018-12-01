c65893fc1dc174980657c7364bf3b480affc789a
Khai Do <zaro0508@gmail.com>
2018-11-30 16:07:18 -0800

    pin sceptre to version 1.x (#18)

--- ci/setup_sceptre.sh
+++ ci/setup_sceptre.sh
@@ -2,7 +2,7 @@
 set -ex
 
 # install app
-pip install --exists-action a "sceptre>=1.3"
+pip install --exists-action a "sceptre<2.0"
 # install contributions
 mkdir resolvers
 wget https://raw.githubusercontent.com/cloudreach/sceptre/v1/contrib/ssm-resolver/ssm.py -O resolvers/ssm.py
