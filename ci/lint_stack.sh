f38ec04d8f2527ffb36fa6e04a3a454f8e4b279c
Khai Do <zaro0508@gmail.com>
2019-04-01 13:16:37 -0700

    fix path

--- ci/lint_stack.sh
+++ ci/lint_stack.sh
@@ -36,7 +36,7 @@ get_cf_stack_names() {
 
 # Get the newly added stack_name
 get_new_stack_name() {
-  diff_output=$(git diff HEAD~1 | grep '+stack_name:' || true)
+  diff_output=$(/usr/bin/git diff HEAD~1 | grep '+stack_name:' || true)
   new_stack_name=${diff_output:13}
   # echo "${new_stack_name}"
 }
@@ -100,9 +100,9 @@ while getopts ":rl:" options; do
        if [ ! -z "${new_stack_name}" ]; then
          verify_name_constraint
          # get all stack names from the last commit
-         ( git checkout HEAD~1 ) 2> /dev/null
+         ( /usr/bin/git checkout HEAD~1 ) 2> /dev/null
          get_local_stack_names
-         ( git checkout - ) 2> /dev/null
+         ( /usr/bin/git checkout - ) 2> /dev/null
          verify_unique
        fi
       ;;
