665c525e85d6e8721429bc5d5a146103939a6dc0
Khai Do <3697686+zaro0508@users.noreply.github.com>
2019-04-12 23:06:54 -0700

    script to associate win instance to jumpcloud systems (#28)

---
+++ aws/associate-jc-system.ps1
@@ -0,0 +1,16 @@
+# Associate a Windows EC2 instance with a jumpcloud system group
+# Usage:
+#   associate-jc-system.ps1 -JcServiceApiKey XXXXXXXXXX -JcSystemsGroupId XXXXXXXXX
+
+# Get jumpcloud info from flag or env var
+Param(
+    [String]$JcSystemsGroupId = $env:JC_SYSTEMS_GROUP_ID,
+    [String]$JcServiceApiKey = $env:JC_SERVICE_API_KEY
+)
+if(-not($JcSystemsGroupId)) { Throw "-JcSystemsGroupId is required" }
+if(-not($JcServiceApiKey)) { Throw "-JcServiceApiKey is required" }
+
+Install-Module -Name JumpCloud -Force
+Connect-JCOnline $JcServiceApiKey -force
+$JcSystemId = Get-Content $env:ProgramFiles\JumpCloud\Plugins\Contrib\jcagent.conf|jq -r '.systemKey'
+Add-JCSystemGroupMember -SystemID $JcSystemId -ByID -GroupID $JcSystemsGroupId
