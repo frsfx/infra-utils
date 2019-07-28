402277e483ad9e2afb4ba6db01d614b8ad9fb46d
Khai Do <3697686+zaro0508@users.noreply.github.com>
2019-07-27 22:39:21 -0700

    Update windows MSVC++ install script (#39)
    The jumpcloud agent has been updated from a 32 bit (x86) to
    64 bit (x64) app.  The app used to requires 32 MS visual C++
    but now it requires the 64 bit version of MSVC++.  The
    change to the installer script will install both versions
    on windows.

--- aws/install-ms-vc.ps1
+++ aws/install-ms-vc.ps1
@@ -10,11 +10,20 @@ Function InstallVcRedist() {
 
     # Get and install VC++ 2013 Redistributable package (x86 and x64)
     New-Item $env:TEMP\VcRedist -ItemType Directory -Force
-    $VcList = Get-VcList -Release 2013
+
+    $VcList = Get-VcList -Release 2013 -Architecture x86
     $VcList | Save-VcRedist -ForceWebRequest -Path $env:TEMP\VcRedist
     Install-VcRedist -Silent -Path $env:TEMP\VcRedist -VcList $VcList
+
+    $VcList = Get-VcList -Release 2013 -Architecture x64
+    $VcList | Save-VcRedist -ForceWebRequest -Path $env:TEMP\VcRedist
+    Install-VcRedist -Silent -Path $env:TEMP\VcRedist -VcList $VcList
+
+    # verify installed VC++
     Write-Host "Installed VC++ Distributions: "
     Get-InstalledVcRedist | Select Name, Version, ProductCode
+
 }
 
 InstallVcRedist
+
