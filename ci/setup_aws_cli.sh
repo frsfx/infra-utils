fae09c830c1938f59786ab0461905d7599df3437
T. Thyer <tess.thyer@sagebase.org>
2020-03-17 15:28:39 -0700

    Add -p option to mkdir (#42)
    Co-authored-by: T. Thyer <tess.thyer@sagebionetworks.org>

--- ci/setup_aws_cli.sh
+++ ci/setup_aws_cli.sh
@@ -12,6 +12,6 @@ fi
 
 # setup the awscli with the correct aws account info.
 pip install awscli
-mkdir ~/.aws
+mkdir -p ~/.aws
 echo -e "[default]\nregion=us-east-1\nsource_profile=default\nrole_arn=$AwsCfServiceRoleArn" > ~/.aws/config
 echo -e "[default]\nregion=us-east-1\naws_access_key_id=$AwsTravisAccessKey\naws_secret_access_key=$AwsTravisSecretAccessKey" > ~/.aws/credentials
